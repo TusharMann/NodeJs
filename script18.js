b7e47abc5f7769a216c5d549baafd580095cef72
Tushar Mann <tusharmann29@gmail.com>
2017-03-19 03:25:28 +0530

    getrequest

---
+++ script18.js
@@ -0,0 +1,15 @@
+var express=require('express');
+var path=require('path');
+var app=express();
+
+app.use('/cssFiles',express.static(__dirname+'/files'));
+
+app.get('/', function(request,response){
+	// body...
+	//response.sendFile('index.html',{root: path.join(__dirname,'./files')}) ;
+	response.end(JSON.stringify(request.query))	;
+})
+
+app.listen(1337,function() {
+	console.log('Listening at port 1337');
+});

e76d003b54b751f0a900845a2d5b8c926f4fb2ba
Tushar Mann <tusharmann29@gmail.com>
2017-03-19 03:00:53 +0530

    simple html server

--- script14.js
+++ script14.js
@@ -1,12 +1,14 @@
 var express=require('express');
+var path=require('path');
 var app=express();
-var path=express('path');
+
+app.use('/cssFiles',express.static(__dirname+'/files'));
 
 app.get('/', function(request,response){
 	// body...
-	response.sendFile('index.html',{root:path.join(_dirname,'./files')}) ;
+	response.sendFile('index.html',{root: path.join(__dirname,'./files')}) ;
 })
 
 app.listen(1337,function() {
-	console.log('Listeing at port 1337');
+	console.log('Listening at port 1337');
 });

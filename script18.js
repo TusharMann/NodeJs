02da389edf2eca74d94d9400d1d42b07f6596e03
Tushar Mann <tusharmann29@gmail.com>
2017-03-19 03:41:45 +0530

    post request

--- script18.js
+++ script18.js
@@ -1,14 +1,21 @@
 var express=require('express');
 var path=require('path');
 var app=express();
+var bodyparser=require('body-parser');
+
+app.use(bodyparser());
 
 app.use('/cssFiles',express.static(__dirname+'/files'));
 
 app.get('/', function(request,response){
 	// body...
-	//response.sendFile('index.html',{root: path.join(__dirname,'./files')}) ;
-	response.end(JSON.stringify(request.query))	;
-})
+	response.sendFile('index.html',{root: path.join(__dirname,'./files')}) ;
+	//response.end(JSON.stringify(request.query))	;
+});
+
+app.post('/',function(request,response){
+	response.end(JSON.stringify(request.body));
+});
 
 app.listen(1337,function() {
 	console.log('Listening at port 1337');

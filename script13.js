dd15a999ee5f0584c4edc10377564ada5018759f
Tushar Mann <tusharmann29@gmail.com>
2017-03-19 02:26:05 +0530

    serving static files

--- script13.js
+++ script13.js
@@ -2,6 +2,15 @@ var express=require('express');
 
 var app=express();
 
+app.use('/cssFiles',express.static(__dirname+'/files'));
+
+app.get('/hello', function(request,response){
+	// body...
+	//response.end('test');
+	response.send('hello everyone') 
+})
+
+
 app.listen(1337,function() {
 	console.log('Listeing at port 1337');
 });
